8a53b5a79e7c79d837188c650c926cee253dc39b
sdinkle <sdinkle88@gmail.com>
2019-08-20 19:30:31 -0400

    Add entry point app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Global node requires
+// nothing yet
+
+// Third-party npm requires
+const express = require('express');
+
+// Local requires
+// nothing yet
+
+// Spin up the Express app by calling the express() function.
+const app = express();
+
+// Establish the routes.
+
+// Mount the middleware functions.
+
+// Listen on port 3000.
+app.listen(3000);
